2520e604a9dd6b778ac4a4038deb03746d4afaee
Alex Sergeyev <asergeyev@dyn.com>
2012-02-14 11:00:17 -0500

    Perldoc changes to mention authorship

--- lib/Digest/MurmurHash.pm
+++ lib/Digest/MurmurHash.pm
@@ -55,10 +55,13 @@ Toru Maesaka, E<lt>dev@torum.netE<gt>
 
 Copyright (C) 2008 by Toru Maesaka
 
-MurmurHash algorithm comes from Austin Appleby.
+MurmurHash algorithm and code in this distribution from Austin Appleby.
+
+Changes to support MurmurHash3 functions are from Alex Sergeyev.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
+
